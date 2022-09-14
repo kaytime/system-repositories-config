fe1ce4eaec4bbe575b50a9c9371b27524da868d2
Steph DGenius <stephanetse@gmail.com>
2022-09-14 02:35:46 +0100

    0.1.0-alpha

--- gh-build.sh
+++ gh-build.sh
@@ -8,8 +8,13 @@ apt -qq -yy install equivs git devscripts lintian --no-install-recommends
 ### Install Dependencies
 mk-build-deps -i -t "apt-get --yes" -r
 
+chmod -x "debian/install"
+
 ### Build Deb
-mkdir source
-mv ./* source/ # Hack for debuild
-cd source
 debuild -b -uc -us
+
+ls -a
+
+### Move Deb to current directory because debuild decided
+### that it was a GREAT IDEA TO PUT THE FILE ONE LEVEL ABOVE
+mv ../*.deb .
